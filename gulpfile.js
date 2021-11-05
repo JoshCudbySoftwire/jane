ed2456546374dbd00bae027141a5229b3eb6f411
Lawrence Tray <lawrence.tray@softwire.com>
2021-11-05 12:30:54 +0000

    gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var babel = require("gulp-babel");
+var run = require('gulp-run');
 
 gulp.task('build', function() {
   return gulp.src("src/**/*.js")
@@ -12,8 +13,22 @@ gulp.task('dev', gulp.series('build', function() {
     return nodemon({
         script: 'dist/index.js',
         ext: 'js',
-        ignore: ['dist/'],
+        ignore: ['dist/', 'test/'],
         env: { 'NODE_ENV': 'development' },
-        tasks: ['build']
+        tasks: ['build', 'test']
     });
 }));
+
+gulp.task('test', () => {
+    return run("mocha test/**/*.js --require babel-core/register").exec();
+})
+
+gulp.task('background-test', (done) => {
+    return nodemon({
+        script: "banter.js",
+        ext: 'js',
+        ignore: ['dist/'],
+        tasks: ['test'],
+        done: done
+    });
+});
