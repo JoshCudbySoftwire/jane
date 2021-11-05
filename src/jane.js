01aa91628e4c49743a03501c9d2f4d4941d95526
Lawrence Tray <lawrence.tray@softwire.com>
2021-11-05 11:28:29 +0000

    Beauty

--- src/jane.js
+++ src/jane.js
@@ -1,18 +1,20 @@
 import sharp from "sharp";
-import fs from "fs";
 import readline from "readline";
+import { exit } from "process";
 
 const circleMask = Buffer.from(
 	'<svg><rect x="0" y="0" width="200" height="200" rx="100" ry="100"/></svg>'
 )
 
-const roundedCornerResizer = sharp()
+const roundedCornerResizer = (input, output) => sharp(input)
 	.resize(200, 200)
 	.composite([{
 		input: circleMask,
 		blend: 'dest-in'
 	}])
-	.png();
+	.png()
+	.toFile(output)
+	.then(() => console.log("Success!"));
 
 
 const rl = readline.createInterface(({
@@ -22,17 +24,20 @@ const rl = readline.createInterface(({
 }));
 
 const roundify = (file) => {
-	const inputFile = `input/${file}`;
-	const outputFile = `output/${inputFile}.rounded.png`
-	const stream = fs.createReadStream(inputFile)
-		.pipe(roundedCornerResizer)
-		.pipe(fs.createWriteStream(outputFile));
-
-	stream.on("finish", () => {
-		console.log("Success!");
-		rl.close();
-	});
-	stream.on("error", (error) => console.log(`Oh no! My error: ${error}`))
+	if (file === "q") {
+		console.log("Quitting...");
+		exit();
+	} else {
+		const inputFile = `input/${file}`;
+		const outputFile = `output/${file}.rounded.png`;
+		roundedCornerResizer(inputFile, outputFile)
+			.then(exit)
+			.catch((err) => {
+				console.log(err);
+				rl.question("Try again: ", roundify);
+			});
+	}
 }
 
+console.log("Welcome to jane.js v3.2.17\nPress (q) to exit\n")
 rl.question("File to roundify: ", roundify);
